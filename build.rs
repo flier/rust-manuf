257f162967f4889a4c618a229e4b765f6e61d61c
Flier Lu <flier.lu@gmail.com>
2020-05-20 18:37:15 +0800

    fetch latest manuf file on build time

--- build.rs
+++ build.rs
@@ -2,27 +2,39 @@ extern crate itertools;
 
 use std::env;
 use std::fs::File;
-use std::io::{BufReader, Write};
+use std::io::{self, BufReader, Write};
 use std::path::Path;
 
 include!("src/parse.rs");
 
-fn main() {
-    let f = File::open("src/manuf").unwrap();
-    let r = BufReader::new(f);
+#[cfg(feature = "latest")]
+fn manuf() -> Result<BufReader<io::Cursor<String>>, anyhow::Error> {
+    const LATEST_MANUF_URL: &str =
+        "https://code.wireshark.org/review/gitweb?p=wireshark.git;a=blob_plain;f=manuf;hb=HEAD";
 
-    let out_dir = env::var("OUT_DIR").unwrap();
+    let text = reqwest::blocking::get(LATEST_MANUF_URL)?.text()?;
+
+    Ok(BufReader::new(io::Cursor::new(text)))
+}
+
+#[cfg(not(feature = "latest"))]
+fn manuf() -> Result<BufReader<File>, io::Error> {
+    File::open("src/manuf").map(BufReader::new)
+}
+
+fn main() -> Result<(), anyhow::Error> {
+    let manuf = manuf()?;
+    let out_dir = env::var("OUT_DIR")?;
     let dest_path = Path::new(&out_dir).join("vendors.rs");
-    let mut out = File::create(&dest_path).unwrap();
+    let mut out = File::create(&dest_path)?;
 
     out.write_all(
         b"#[doc(hidden)]
 #[allow(clippy::unreadable_literal, clippy::type_complexity)]
 pub const VENDORS: &[((EtherAddr, u64), (&str, &str))] = &[\n",
-    )
-    .unwrap();
+    )?;
 
-    for ((prefix, prefix_len), (name, desc)) in parse(r) {
+    for ((prefix, prefix_len), (name, desc)) in parse(manuf) {
         let prefix_str = itertools::join(prefix.iter().map(|n| format!("0x{:02x}", n)), ", ");
         let prefix_mask = ((1u64 << prefix_len) - 1) << (48 - prefix_len);
 
@@ -32,9 +44,10 @@ pub const VENDORS: &[((EtherAddr, u64), (&str, &str))] = &[\n",
                 prefix_str, prefix_mask, name, desc
             )
             .as_bytes(),
-        )
-        .unwrap();
+        )?;
     }
 
-    out.write_all(b"];\n").unwrap();
+    out.write_all(b"];\n")?;
+
+    Ok(())
 }
